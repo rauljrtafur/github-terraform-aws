02a6e39db4e58e00992bdad79d61941aad665057
rauljrtafur95 <raul.tafur@rappi.com>
2024-04-01 17:43:40 -0500

    modification providers.tf

--- main.tf
+++ main.tf
@@ -2,13 +2,8 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~> 3.0"
     }
-  }
-  backend "s3" {
-    bucket = "github-terraform-aws-dev" # change to name of your bucket
-    region = "us-east-1"                # change to your region
-    key    = "infra.tfstate"
   }
 }
 
