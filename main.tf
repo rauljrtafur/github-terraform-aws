50d8ce3664f4ef509818378a38d9e82e10d09dd3
rauljrtafur95 <raul.tafur@rappi.com>
2024-04-01 21:11:08 -0500

    add provider attri

--- main.tf
+++ main.tf
@@ -10,6 +10,8 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
+  access_key = "AKIAW3MEDYFJK7BYYGEP"
+  secret_key = "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"
 }
 
 
